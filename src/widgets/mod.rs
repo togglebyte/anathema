7735e1de912abddab05c565452f7862b86b696a3
Togglebit <me@togglebit.io>
2021-10-17 14:58:43 +0200

    dirty updates

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -7,52 +7,58 @@ pub struct ScrollBuffer<T> {
     height: usize,
     pos: usize,
     max_buffer: usize,
+    dirty: bool,
 }
 
 impl<T> ScrollBuffer<T> {
     pub fn new(height: usize, max_buffer: usize) -> Self {
         let mut entries = VecDeque::new();
         entries.make_contiguous();
-        Self { entries, height, pos: 0, max_buffer }
+        Self { entries, height, pos: 0, max_buffer, dirty: true }
     }
 
     pub fn from_vec(entries: Vec<T>, height: usize, max_buffer: usize) -> Self {
         let mut entries: VecDeque<T> = entries.into();
         entries.make_contiguous();
-        Self { entries: entries.into(), height, pos: 0, max_buffer }
+        Self { entries: entries.into(), height, pos: 0, max_buffer, dirty: true }
     }
 
     fn is_at_end(&self) -> bool {
         self.pos == self.entries.len() - self.height
     }
 
-    pub fn lines(&self) -> &[T] {
-        let to = (self.pos + self.height).min(self.entries.len());
-        let from = self.pos.min(to.saturating_sub(self.height));
-        &self.entries.as_slices().0[from..to]
+    pub fn lines(&mut self) -> &[T] {
+        if self.dirty {
+            let to = (self.pos + self.height).min(self.entries.len());
+            let from = self.pos.min(to.saturating_sub(self.height));
+            self.dirty = false;
+            &self.entries.as_slices().0[from..to]
+        } else {
+            &[]
+        }
     }
 
-    pub fn resize(&mut self, new_heigth: usize) -> &[T] {
+    pub fn resize(&mut self, new_heigth: usize) {
         self.height = new_heigth;
-        self.lines()
+        self.dirty = true;
     }
 
-    pub fn scroll_down(&mut self, lines: usize) -> &[T] {
+    pub fn scroll_down(&mut self, lines: usize) {
         self.pos = (self.pos + lines).min(self.entries.len().saturating_sub(self.height));
-        self.lines()
+        self.dirty = true;
     }
 
-    pub fn scroll_up(&mut self, lines: usize) -> &[T] {
+    pub fn scroll_up(&mut self, lines: usize) {
         self.pos = self.pos.saturating_sub(lines);
-        self.lines()
+        self.dirty = true;
     }
 
-    pub fn scroll_to_end(&mut self) -> &[T] {
+    pub fn scroll_to_end(&mut self) {
         self.pos = self.entries.len().saturating_sub(self.height);
-        self.lines()
+        self.dirty = true;
     }
 
-    pub fn push(&mut self, entry: T) -> &[T] {
+    pub fn push(&mut self, entry: T) {
         if self.entries.len() == self.max_buffer {
             self.entries.pop_front();
         }
@@ -65,5 +71,9 @@ impl<T> ScrollBuffer<T> {
             true => self.scroll_down(1),
             false => self.scroll_up(0),
         }
+    }
+
+    pub fn is_dirty(&self) -> bool {
+        self.dirty
     }
 }
