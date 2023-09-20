91b8413e194726ca319b0f4057e7bb6569f6e9fe
Togglebit <me@togglebit.io>
2023-09-20 14:32:27 +0200

    this is compiling, it might look like it's not, but it is

--- anathema-vm/src/scope.rs
+++ anathema-vm/src/scope.rs
@@ -1,6 +1,8 @@
 use anathema_compiler::{Constants, Instruction, StringId};
-use anathema_values::ScopeValue;
-use anathema_widget_core::generator::{Attributes, Expression, Loop, SingleNode, If, Else, ControlFlow};
+use anathema_values::{ScopeValue, ValueExpr};
+use anathema_widget_core::generator::{
+    Attributes, ControlFlow, Else, Expression, If, Loop, SingleNode,
+};
 
 use crate::error::Result;
 
@@ -61,15 +63,15 @@ impl<'vm> Scope<'vm> {
                     let body = Scope::new(body, &self.consts).exec()?;
 
                     let mut control_flow = ControlFlow {
-                        if_expr: If {
-                            cond,
-                            body,
-                        },
+                        if_expr: If { cond, body },
                         elses: vec![],
                     };
 
                     loop {
-                        let Some(&Instruction::Else { cond, size }) = self.instructions.get(0) else { break; };
+                        let Some(&Instruction::Else { cond, size }) = self.instructions.get(0)
+                        else {
+                            break;
+                        };
                         let cond = cond.map(|cond| self.consts.lookup_value(cond));
 
                         let body = self.instructions.drain(..size).collect();
@@ -98,39 +100,38 @@ impl<'vm> Scope<'vm> {
     }
 
     fn node(&mut self, ident: StringId, scope_size: usize) -> Result<Expression> {
-        panic!()
-        // let ident = self.consts.lookup_string(ident);
+        let ident = self.consts.lookup_string(ident);
 
-        // let mut attributes = Attributes::new();
-        // let mut text = None::<ScopeValue>;
-        // let mut ip = 0;
+        let mut attributes = Attributes::new();
+        let mut text = None::<ValueExpr>;
+        let mut ip = 0;
 
-        // loop {
-        //     match self.instructions.get(ip) {
-        //         Some(Instruction::LoadAttribute { key, value }) => {
-        //             let key = self.consts.lookup_string(*key);
-        //             let value = self.consts.lookup_value(*value);
-        //             attributes.insert(key.to_string(), value.clone());
-        //         }
-        //         Some(Instruction::LoadText(i)) => text = Some(self.consts.lookup_value(*i).clone()),
-        //         _ => break,
-        //     }
-        //     ip += 1;
-        // }
+        loop {
+            match self.instructions.get(ip) {
+                Some(Instruction::LoadAttribute { key, value }) => {
+                    let key = self.consts.lookup_string(*key);
+                    let value = self.consts.lookup_value(*value);
+                    attributes.insert(key.to_string(), value.clone());
+                }
+                Some(Instruction::LoadText(i)) => text = Some(self.consts.lookup_value(*i).clone()),
+                _ => break,
+            }
+            ip += 1;
+        }
 
-        // // Remove processed attribute and text instructions
-        // self.instructions.drain(..ip);
+        // Remove processed attribute and text instructions
+        self.instructions.drain(..ip);
 
-        // let scope = self.instructions.drain(..scope_size).collect();
-        // let children = Scope::new(scope, &self.consts).exec()?;
+        let scope = self.instructions.drain(..scope_size).collect();
+        let children = Scope::new(scope, &self.consts).exec()?;
 
-        // let node = Expression::Node(SingleNode {
-        //     ident: ident.to_string(),
-        //     text,
-        //     attributes,
-        //     children: children.into(),
-        // });
+        let node = Expression::Node(SingleNode {
+            ident: ident.to_string(),
+            text,
+            attributes,
+            children: children.into(),
+        });
 
-        // Ok(node)
+        Ok(node)
     }
 }
