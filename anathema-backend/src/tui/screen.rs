88c53cbc0c1af651655bea6ba8febbd60fb4e2ba
LondoSpark <ridecar2@gmail.com>
2024-08-30 21:07:30 +0100

    Fix lint error

--- anathema-backend/src/tui/screen.rs
+++ anathema-backend/src/tui/screen.rs
@@ -3,9 +3,9 @@ use std::io::{Result, Write};
 use anathema_geometry::{Pos, Size};
 use anathema_widgets::paint::CellAttributes;
 use anathema_widgets::WidgetRenderer;
-use crossterm::event::{DisableMouseCapture, EnableMouseCapture};
+use crossterm::event::EnableMouseCapture;
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::{cursor, ExecutableCommand, QueueableCommand};
+use crossterm::{cursor, ExecutableCommand};
 
 use super::buffer::{diff, draw_changes, Buffer, Change};
 use super::{LocalPos, Style};
