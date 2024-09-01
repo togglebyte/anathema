b4298692f6cc5bd4c7a2294d1e2030e2db701469
Willians Faria <dev.willians.faria@gmail.com>
2024-09-01 15:09:03 -0300

    fixing wrong behavior when expanding without available space

--- anathema-default-widgets/src/layout/many.rs
+++ anathema-default-widgets/src/layout/many.rs
@@ -113,8 +113,8 @@ impl Many {
             }
         });
 
-        // Apply spacer and expand if the layout is constrained
-        if !self.unconstrained {
+        // Apply spacer and expand if the layout is constrained and we have remaining space
+        if !self.unconstrained && !self.used_size.no_space_left() {
             let constraints = self.used_size.to_constraints();
             let expanded_size = expand::layout_all_expansions(&mut children, constraints, self.axis, ctx);
             self.used_size.apply(expanded_size);
