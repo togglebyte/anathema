6a9cb2c60ea735f13fe4cb42ec6b61c367b5639f
doddi <mark.dodgson@googlemail.com>
2024-09-23 08:36:07 +0100

    Fixed screen test

--- anathema-backend/src/tui/screen.rs
+++ anathema-backend/src/tui/screen.rs
@@ -205,12 +205,16 @@ mod test {
     }
 
     #[test]
-    #[should_panic(expected = "index out of bounds: the len is 1 but the index is 4")]
     fn put_outside_of_screen() {
         // Put a character outside of the screen should panic
         let glyph_map = GlyphMap::empty();
         let mut screen = make_screen(Size::new(1, 1));
         screen.paint_glyph(Glyph::from_char('x', 1), LocalPos::new(2, 2));
         screen.render(&mut vec![], &glyph_map).unwrap();
+
+        // Check no content was overwritten
+        for ele in screen.new_buffer.inner {
+            assert_eq!(Cell::new(Glyph::from_char('0', 1), Style::reset()), ele)
+        }
     }
 }
