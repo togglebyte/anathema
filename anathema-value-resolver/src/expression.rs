e3a68d6c819ccfff4deb93971388b37df8c32565
Togglebit <me@togglebit.io>
2025-01-11 20:24:37 +0100

    more tests

--- anathema-value-resolver/src/expression.rs
+++ anathema-value-resolver/src/expression.rs
@@ -79,7 +79,6 @@ pub enum ValueExpr<'bp> {
     Op(Box<Self>, Box<Self>, Op),
     Either(Box<Self>, Box<Self>),
 
-    // Dyn(ValueRef),
     Call,
 
     Null,
@@ -325,5 +324,423 @@ fn float_op(lhs: f64, rhs: f64, op: Op) -> f64 {
         Op::Div => lhs / rhs,
         Op::Mul => lhs * rhs,
         Op::Mod => lhs % rhs,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use anathema_state::Hex;
+    use anathema_templates::expressions::{
+        and, boolean, chr, either, eq, float, greater_than, greater_than_equal, hex, ident, index, less_than,
+        less_than_equal, map, neg, not, num, or, strlit,
+    };
+    use anathema_state::{AnyValue, List, Map, States};
+    use anathema_templates::expressions::{add, div, modulo, mul, sub};
+    use anathema_templates::{Globals, Variables};
+
+    use super::*;
+    use crate::context::ResolverCtx;
+    use crate::immediate::ImmediateResolver;
+    use crate::scope::Scope;
+    use crate::value::Value;
+    use crate::Resolver;
+
+    struct TestCaseBuilder {
+        variables: Variables,
+        scopes: Scope,
+        states: States,
+        state: Map<Box<dyn AnyValue>>,
+    }
+
+    impl TestCaseBuilder {
+        pub fn new(scopes: Scope, states: States) -> Self {
+            Self {
+                variables: Variables::new(),
+                scopes,
+                states,
+                state: Map::empty(),
+            }
+        }
+
+        pub fn with_global(mut self, key: &str, value: impl Into<Expression>) -> Self {
+            self.variables.declare(key, value);
+            self
+        }
+
+        pub fn with_state(mut self, key: &str, value: impl AnyValue) -> Self {
+            let value: Box<dyn AnyValue> = Box::new(value);
+            self.state.insert(key, value);
+            self
+        }
+
+        pub fn finish(mut self) -> TestCase {
+            let globals = self.variables.into();
+            let state_id = self.states.insert(self.state);
+            self.scopes.insert_state(state_id);
+            TestCase {
+                globals,
+                scopes: self.scopes,
+                states: self.states,
+            }
+        }
+    }
+
+    struct TestCase {
+        globals: Globals,
+        scopes: Scope,
+        states: States,
+    }
+
+    impl TestCase {
+        fn eval<'bp>(&'bp self, expr: &'bp Expression) -> Value<'bp> {
+            let ctx = ResolverCtx::new(&self.globals, &self.scopes, &self.states);
+            let mut resolver = ImmediateResolver::new(&ctx);
+            let value_expr = resolver.resolve(expr);
+            let val = Value::new(value_expr, Subscriber::ZERO);
+            val
+        }
+    }
+
+    fn setup() -> TestCaseBuilder {
+        let mut scopes = Scope::new();
+        let mut states = States::new();
+        TestCaseBuilder::new(scopes, states)
+    }
+
+    #[test]
+    fn either_index() {
+        // state[0] ? attributes[0]
+        let expr = either(
+            index(index(ident("attributes"), strlit("a")), num(0)),
+            index(index(ident("state"), strlit("a")), num(0)),
+        );
+
+        let mut list = List::empty();
+        list.push("a string");
+
+        let test = setup().with_state("a", list).finish();
+        let value = test.eval(&*expr);
+        assert_eq!("a string", value.to_str().unwrap());
+    }
+
+    #[test]
+    fn either_then_index() {
+        // (state ? attributes)[0]
+        let expr = index(
+            either(
+                index(ident("attributes"), strlit("a")),
+                index(ident("state"), strlit("a")),
+            ),
+            num(0),
+        );
+
+        let mut list = List::empty();
+        list.push("a string");
+
+        let test = setup().with_state("a", list).finish();
+        let value = test.eval(&*expr);
+        assert_eq!("a string", value.to_str().unwrap());
+    }
+
+    #[test]
+    fn either_or() {
+        let test = setup().with_state("a", 1).with_state("b", 2).finish();
+
+        // There is no c, so use b
+        let expr = either(index(ident("state"), strlit("c")), index(ident("state"), strlit("b")));
+        let value = test.eval(&*expr);
+        assert_eq!(2, value.to_int().unwrap());
+
+        // There is a, so don't use b
+        let expr = either(index(ident("state"), strlit("a")), index(ident("state"), strlit("b")));
+        let value = test.eval(&*expr);
+        assert_eq!(1, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn mods() {
+        let test = setup().with_state("num", 5).finish();
+        let lookup = index(ident("state"), strlit("num"));
+        let expr = modulo(lookup, num(3));
+        let value = test.eval(&*expr);
+        assert_eq!(2, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn division() {
+        let test = setup().with_state("num", 6).finish();
+        let lookup = index(ident("state"), strlit("num"));
+        let expr = div(lookup, num(2));
+        let value = test.eval(&*expr);
+        assert_eq!(3, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn multiplication() {
+        let test = setup().with_state("num", 2).finish();
+        let lookup = index(ident("state"), strlit("num"));
+        let expr = mul(lookup, num(2));
+        let value = test.eval(&*expr);
+        assert_eq!(4, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn subtraction() {
+        let test = setup().with_state("num", 1).finish();
+        let lookup = index(ident("state"), strlit("num"));
+        let expr = sub(lookup, num(2));
+        let value = test.eval(&*expr);
+        assert_eq!(-1, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn addition() {
+        let test = setup().with_state("num", 1).finish();
+        let lookup = index(ident("state"), strlit("num"));
+        let expr = add(lookup, num(2));
+        let value = test.eval(&*expr);
+        assert_eq!(3, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_or() {
+        let test = setup().finish();
+        let is_true = or(boolean(false), boolean(true));
+        let is_true = test.eval(&*is_true);
+        assert_eq!(true, is_true.to_bool().unwrap());
+    }
+
+    #[test]
+    fn test_and() {
+        let test = setup().finish();
+        let is_true = and(boolean(true), boolean(true));
+        let is_true = test.eval(&*is_true);
+        assert_eq!(true, is_true.to_bool().unwrap());
+    }
+
+    #[test]
+    fn lte() {
+        let test = setup().finish();
+        let is_true = less_than_equal(num(1), num(2));
+        let is_also_true = less_than_equal(num(1), num(1));
+        let is_true = test.eval(&*is_true);
+        let is_also_true = test.eval(&*is_also_true);
+        assert_eq!(true, is_true.to_bool().unwrap());
+        assert_eq!(true, is_also_true.to_bool().unwrap());
+    }
+
+    #[test]
+    fn lt() {
+        let test = setup().finish();
+        let is_true = less_than(num(1), num(2));
+        let is_false = less_than(num(1), num(1));
+        let is_true = test.eval(&*is_true);
+        let is_false = test.eval(&*is_false);
+        assert_eq!(true, is_true.to_bool().unwrap());
+        assert_eq!(false, is_false.to_bool().unwrap());
+    }
+
+    #[test]
+    fn gte() {
+        let test = setup().finish();
+        let is_true = greater_than_equal(num(2), num(1));
+        let is_also_true = greater_than_equal(num(2), num(2));
+        let is_true = test.eval(&*is_true);
+        let is_also_true = test.eval(&*is_also_true);
+        assert_eq!(true, is_true.to_bool().unwrap());
+        assert_eq!(true, is_also_true.to_bool().unwrap());
+    }
+
+    #[test]
+    fn gt() {
+        let test = setup().finish();
+        let is_true = greater_than(num(2), num(1));
+        let is_false = greater_than(num(2), num(2));
+        let is_true = test.eval(&*is_true);
+        let is_false = test.eval(&*is_false);
+        assert_eq!(true, is_true.to_bool().unwrap());
+        assert_eq!(false, is_false.to_bool().unwrap());
+    }
+
+    #[test]
+    fn equality() {
+        let test = setup().finish();
+        let is_true = eq(num(1), num(1));
+        let is_true = test.eval(&is_true);
+        let is_false = &not(eq(num(1), num(1)));
+        let is_false = test.eval(is_false);
+        assert_eq!(true, is_true.to_bool().unwrap());
+        assert_eq!(false, is_false.to_bool().unwrap());
+    }
+
+    #[test]
+    fn neg_float() {
+        let expr = neg(float(123.1));
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(-123.1, value.to_float().unwrap());
+    }
+
+    #[test]
+    fn neg_num() {
+        let expr = neg(num(123));
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(-123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn not_true() {
+        let expr = not(boolean(false));
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(true, value.to_bool().unwrap());
+    }
+
+    #[test]
+    fn str_resolve() {
+        // state[empty|full]
+        let expr = index(ident("state"), either(ident("empty"), ident("full")));
+        let test = setup()
+            .with_state("key", "a string")
+            .with_global("full", "key")
+            .finish();
+        let value = test.eval(&*expr);
+        assert_eq!("a string", value.to_str().unwrap());
+    }
+
+    #[test]
+    fn state_string() {
+        let expr = index(ident("state"), strlit("str"));
+        let test = setup().with_state("str", "a string").finish();
+        let value = test.eval(&*expr);
+        assert_eq!("a string", value.to_str().unwrap());
+    }
+
+    #[test]
+    fn state_float() {
+        let expr = index(ident("state"), strlit("float"));
+        let test = setup().with_state("float", 1.2).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(1.2, value.to_float().unwrap());
+    }
+
+    #[test]
+    fn test_either() {
+        let expr = either(ident("missings"), num(2));
+        let test = setup().with_global("missing", 111).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(2, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_hex() {
+        let expr = hex((1, 2, 3));
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(Hex::from((1, 2, 3)), value.to_hex().unwrap());
+    }
+
+    #[test]
+    fn test_char() {
+        let expr = chr('x');
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!('x', value.to_char().unwrap());
+    }
+
+    #[test]
+    fn test_float() {
+        let expr = float(123.123);
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123.123, value.to_float().unwrap());
+    }
+
+    #[test]
+    fn test_int() {
+        let expr = num(123);
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_bool() {
+        let expr = boolean(true);
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert!(value.to_bool().unwrap());
+    }
+
+    #[test]
+    fn test_dyn_list() {
+        let expr = index(index(ident("state"), strlit("list")), num(1));
+        let mut list = List::empty();
+        list.push(123);
+        list.push(456);
+
+        let test = setup().with_state("list", list).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(456, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_expression_map_state_key() {
+        let expr = index(map([("value", 123)]), index(ident("state"), strlit("key")));
+        let test = setup().with_state("key", "value").finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_expression_map() {
+        let expr = index(map([("value", 123)]), strlit("value"));
+        let test = setup().finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_dyn_map_dyn_key() {
+        let expr = index(ident("state"), strlit("value"));
+        let test = setup().with_state("value", 123).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_dyn_map() {
+        let expr = index(ident("state"), strlit("value"));
+        let test = setup().with_state("value", 123).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_nested_map() {
+        let expr = index(index(ident("state"), strlit("blip")), strlit("value"));
+        let mut inner_map = Map::empty();
+        inner_map.insert("value", 123);
+
+        let test = setup().with_state("blip", inner_map).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
+    }
+
+    #[test]
+    fn test_nested_maps() {
+        let expr = index(
+            index(index(ident("state"), strlit("value")), strlit("value")),
+            strlit("value"),
+        );
+        let mut inner_map = Map::empty();
+        let mut inner_inner_map = Map::empty();
+        inner_inner_map.insert("value", 123);
+        inner_map.insert("value", inner_inner_map);
+
+        let test = setup().with_state("value", inner_map).finish();
+        let value = test.eval(&*expr);
+        assert_eq!(123, value.to_int().unwrap());
     }
 }
