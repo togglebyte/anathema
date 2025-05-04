b3e28f3f5a6d8549550c776f61147a89579c8ce0
museun <museun@users.noreply.github.com>
2025-05-04 13:00:17 -0400

    implement a more friendly derive macro for State
    State can be derived on Unit structs (as Type::Unit)
    State can be derived on Tuple structs (as Type::List)
    
    `#[state_ignore]` still exists but:
    `#[anathema(ignore)]` is a more 'unified' form of it
    
    and I added:
    `#[anathema(rename = "name")]` which can be used
    to rename a field from the `aml` side of things
    
    Note: the attributes cannot be currently used in
    unit/tuple structs.
    
    Example:
    
    ```rust
    use anathema::{State, state::Value}; // TODO prelude names for this
    
    struct AppState {
        #[anathema(ignore)]
        inner: bool
    
        #[anathema(rename = "text")]
        data: Value<String>
    }
    ```
    
    This'll allow the state's data to be referenced as
    `.text` from the template, but stay as 'data' on
    the rust side.

--- anathema-state-derive/src/lib.rs
+++ anathema-state-derive/src/lib.rs
@@ -1,62 +1,31 @@
-use manyhow::{ensure, manyhow, Result};
-use quote_use::quote_use as quote;
-use syn::{self, Data, DeriveInput, Fields};
+use syn::{DeriveInput, parse_macro_input, spanned::Spanned as _};
 
-static STATE_IGNORE: &str = "state_ignore";
+static DERIVE_NAMESPACE: &str = "anathema";
 
-#[manyhow]
-#[proc_macro_derive(State, attributes(state_ignore))]
-pub fn state_derive(input: DeriveInput) -> Result {
-    let name = &input.ident;
+#[proc_macro_derive(State, attributes(anathema, state_ignore))]
+pub fn anathema(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    match &input.data {
+        syn::Data::Struct(data) => structs::generate(&input, data),
 
-    ensure!(let Data::Struct(strct) = &input.data, input, "only structs are supported");
+        syn::Data::Enum(data) => syn::Error::new(
+            data.enum_token.span(), //
+            "anathema's State cannot be derived on enums currently",
+        )
+        .to_compile_error()
+        .into(),
 
-    ensure!(
-        let Fields::Named(struct_fields) = &strct.fields,
-        strct.fields,
-        "only named fields"
-    );
-
-    let (field_idents, field_names): (Vec<_>, Vec<_>) = struct_fields
-        .named
-        .iter()
-        .filter(|f| {
-            // Ignore all `STATE_IGNORE` attributes
-            !f.attrs.iter().any(|attr| attr.path().is_ident(STATE_IGNORE))
-        })
-        .filter_map(|f| f.ident.as_ref())
-        .map(|f| (f, f.to_string()))
-        .unzip();
-
-    Ok(quote! {
-        # use ::anathema::state::{self, AnyMap, Type, Value, ValueRef, PendingValue, Path, state, Subscriber, CommonVal};
-        # use ::std::any::Any;
-
-        impl state::TypeId for #name {
-            const TYPE: Type = Type::Composite;
+        syn::Data::Union(data) => {
+            syn::Error::new(
+                data.union_token.span(), //
+                "anathema's State cannot be derived on unions currently",
+            )
+            .to_compile_error()
+            .into()
         }
-
-        impl state::State for #name {
-            fn type_info(&self) -> Type {
-                Type::Composite
-            }
-
-            fn as_any_map(&self) -> Option<&dyn AnyMap> {
-                Some(self)
-            }
-        }
-
-        impl state::AnyMap for #name {
-            fn lookup(&self, key: &str) -> Option<PendingValue> {
-                match key.as_ref() {
-                    #(
-                        #field_names => {
-                            Some(self.#field_idents.reference())
-                        }
-                    )*
-                    _ => None,
-                }
-            }
-        }
-    })
+    }
 }
+
+mod attributes;
+mod errors;
+mod structs;
