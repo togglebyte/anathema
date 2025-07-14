b83146979949f932c0cb2de41ee51e7d9dc410c3
Togglebit <74262215+togglebyte@users.noreply.github.com>
2025-07-14 15:16:23 +0200

    Update anathema-templates/src/variables.rs

--- anathema-templates/src/variables.rs
+++ anathema-templates/src/variables.rs
@@ -301,7 +301,7 @@ impl Variables {
         self.boundary.pop();
     }
 
-    /// Create a new child and set the new children id as the `current` id.
+    /// Create a new child and set the new child's id as the `current` id.
     /// Any operations done from here on out are acting upon the new child scope.
     pub(crate) fn push(&mut self) {
         let parent = self.root.get_scope_mut(&self.current);
