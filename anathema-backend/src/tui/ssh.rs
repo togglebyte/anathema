15e5a11daa7889ebdbae4ad5a2cfd5829370ffdb
Alexander Hansen <alexanderbh@gmail.com>
2025-07-15 20:50:18 +0200

    exploration

--- anathema-backend/src/tui/ssh.rs
+++ anathema-backend/src/tui/ssh.rs
@@ -1,35 +1,7 @@
-//! The tui module contains the essential types for drawing in the terminal.
-//!
-//! It uses two buffers and only draws the diffs from top left to bottom right, making it less
-//! likely to flicker when moving the cursor etc.
-#![deny(missing_docs)]
-use std::io::{Stdout, Write};
-use std::ops::Add;
-use std::time::Duration;
-
-use anathema_geometry::{LocalPos, Pos, Size};
-use anathema_value_resolver::AttributeStorage;
-use anathema_widgets::components::events::Event;
-pub use anathema_widgets::{Attributes, Style};
-use anathema_widgets::{GlyphMap, PaintChildren, WidgetRenderer};
-use crossterm::cursor::{RestorePosition, SavePosition};
 use crossterm::execute;
-use crossterm::terminal::{BeginSynchronizedUpdate, Clear, ClearType, EndSynchronizedUpdate, size};
-pub use screen::Screen;
-
-pub use self::buffer::Buffer;
-use self::events::Events;
-use crate::Backend;
-
-mod buffer;
-/// Events
-pub mod events;
-mod screen;
-mod ssh;
-mod style;
 
 /// Backend builder for a tui backend.
-pub struct TuiBackendBuilder {
+pub struct SSHBackendBuilder {
     output: Stdout,
     hide_cursor: bool,
     enable_raw_mode: bool,
@@ -37,7 +9,7 @@ pub struct TuiBackendBuilder {
     enable_mouse: bool,
 }
 
-impl TuiBackendBuilder {
+impl SSHBackendBuilder {
     /// Enable an alternative screen.
     /// When using this with stdout it means the output will not persist
     /// once the program exits.
@@ -93,7 +65,7 @@ impl TuiBackendBuilder {
 }
 
 /// Terminal backend
-pub struct TuiBackend {
+pub struct SSHBackend {
     screen: Screen,
     output: Stdout,
     events: Events,
@@ -105,10 +77,10 @@ pub struct TuiBackend {
     enable_mouse: bool,
 }
 
-impl TuiBackend {
+impl SSHBackend {
     /// Create a new instance of the tui backend.
-    pub fn builder() -> TuiBackendBuilder {
-        TuiBackendBuilder {
+    pub fn builder() -> SSHBackendBuilder {
+        SSHBackendBuilder {
             output: std::io::stdout(),
             hide_cursor: false,
             enable_raw_mode: false,
@@ -200,70 +172,5 @@ impl Backend for TuiBackend {
         }
 
         let _ = self.output.flush();
-    }
-}
-
-impl Drop for TuiBackend {
-    fn drop(&mut self) {
-        if self.enable_alt_screen {
-            let _ = execute!(&mut self.output, RestorePosition);
-        }
-        let _ = self.screen.restore(&mut self.output, self.enable_alt_screen);
-    }
-}
-
-/// Represents a position on the screen, meaning this should never
-/// be a value outside of the screen size.
-///
-/// It will be ignored if the value is used in a drawing operation and it's outside the current
-/// screen size.
-///
-/// `Screen::ZERO` is the top left of a buffer.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ScreenPos {
-    /// The x coordinate on screen
-    pub x: u16,
-    /// The y coordinate on screen
-    pub y: u16,
-}
-
-impl ScreenPos {
-    /// A zero screen size
-    pub const ZERO: Self = Self::new(0, 0);
-
-    /// Create a new instance of a `ScreenPos`
-    pub const fn new(x: u16, y: u16) -> Self {
-        Self { x, y }
-    }
-}
-
-impl Add<ScreenPos> for LocalPos {
-    type Output = Self;
-
-    fn add(self, rhs: ScreenPos) -> Self::Output {
-        LocalPos {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-        }
-    }
-}
-
-impl TryFrom<LocalPos> for ScreenPos {
-    type Error = <u16 as TryFrom<usize>>::Error;
-
-    fn try_from(value: LocalPos) -> std::result::Result<ScreenPos, Self::Error> {
-        let x = value.x;
-        let y = value.y;
-        Ok(ScreenPos::new(x, y))
-    }
-}
-
-impl TryFrom<Pos> for ScreenPos {
-    type Error = <i32 as TryFrom<usize>>::Error;
-
-    fn try_from(value: Pos) -> std::result::Result<ScreenPos, Self::Error> {
-        let x: u16 = value.x.try_into()?;
-        let y: u16 = value.y.try_into()?;
-        Ok(ScreenPos::new(x, y))
     }
 }
---
+++ anathema-ssh/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:rustc-env=AWS_LC_SYS_PREBUILT_NASM=1");
+}
